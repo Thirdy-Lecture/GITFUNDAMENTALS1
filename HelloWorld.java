2cbf8f139afb915dd8b5877248ca10045cb30edd
thirdygayares <gayaresthird@gmail.com>
2024-08-24 13:17:31 +0800

    Add Variables and Update Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,37 +1,5 @@
-
-//main class
-
 public class Main {
   public static void main(String[] args) {
-    Person per = new Person();
-    per.setName("Sandro Hobayan");
-    System.out.println("Hello my name is " + per.getName());
+    System.out.println("Hello World");
   }
 }
-
-
-
-
-
-
-//name class
-
-public class Person {
-   private String name;
-
-
-  
-   public String getName() {
-     return name;
-   }
-
-  
-  public void setName(String newName) {
-     this.name = newName;
-   }
-}
-
-                  
-
-
-
