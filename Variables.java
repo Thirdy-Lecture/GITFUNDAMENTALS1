b414c091d0009b4245fbcf484ab32f2241ad6caa
thirdygayares <gayaresthird@gmail.com>
2024-08-24 13:36:53 +0800

    Increase the salary

--- Variables.java
+++ Variables.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     int salary = 15;
-    salary = 20;  // salary is now 20
+    salary = 1000;  // salary is now 20
     System.out.println(myNum);
   }
 }
